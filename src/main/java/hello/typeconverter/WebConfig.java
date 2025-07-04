9d8223a4cfc81e94bce3ac8b0667830470ea318f
mijinB <bmj13465@gmail.com>
2025-07-04 16:30:03 +0900

    포맷터 적용하기

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -1,9 +1,8 @@
 package hello.typeconverter;
 
-import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
-import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,9 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         // 스프링이 내부에서 수 많은 기본 컨버터들을 제공하기 때문에 String ↔ Integer 컨버터는 추가하지 않아도 잘 동작한다.
         // 컨버터를 추가하면 추가한 컨버터가 기본 컨버터 보다 높은 우선순위를 가진다.
+
+        /* 우선순위때문에 주석처리
+           주석한 두개도, 추가한 MyNumberFormatter 도 숫자↔문자 똑같은 건데 컨버터가 더 우선순위가 높아서 포매터가 적용되지 않기 때문.
         registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new IntegerToStringConverter()); */
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
